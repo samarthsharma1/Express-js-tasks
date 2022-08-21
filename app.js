1c79231d80c651f939a3f51152643e796ea08733
SAMARTH SHARMA <108316259+samarthsharma1@users.noreply.github.com>
2022-08-21 08:55:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// const http = require('http');
+const path=require("path");
+const express=require("express");
+const bodyparser= require('body-parser');
+
+const app=express();
+
+const adminroutes=require('./routes/admin');
+const shopRoutes= require('./routes/shop');
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminroutes);
+app.use('/admin',shopRoutes);
+//app.use(adminroutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
+
+/*const server = http.createServer(app);
+server.listen(3000);  */
+app.listen(811); 
